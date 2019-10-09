cf5bd234a7adf1419dff5a4d7430b1d6d066b825
Jun Jiang <smujiang@gmail.com>
2019-10-09 16:59:37 -0500

    Export annotations from QuPath

---
+++ src/wsi_annotation/QuPath_scripts/export_anno_binary_jpgs.groovy
@@ -0,0 +1,104 @@
+/**
+ * Script to export binary masks corresponding to all annotations of an image,
+ * optionally along with extracted image regions.
+ *
+ * Note: Pay attention to the 'downsample' value to control the export resolution!
+ *
+ * @author Pete Bankhead
+ * modified by Jun Jiang (Jiang.Jun@mayo.edu), tested on QuPath 0.2.0-m2
+ */
+
+import qupath.lib.images.servers.ImageServer
+import qupath.lib.objects.PathObject
+import qupath.lib.regions.RegionRequest
+import qupath.lib.roi.PathROIToolsAwt
+import qupath.lib.scripting.QP
+
+import javax.imageio.ImageIO
+import java.awt.Color
+import java.awt.image.BufferedImage
+
+// Get the main QuPath data structures
+def imageData = QP.getCurrentImageData()
+def hierarchy = imageData.getHierarchy()
+def server = imageData.getServer()
+
+// Request all objects from the hierarchy & filter only the annotations
+def annotations = hierarchy.getFlattenedObjectList(null).findAll {it.isAnnotation()}
+
+// Define downsample value for export resolution & output directory, creating directory if necessary
+def downsample = 128.0
+def pathOutput = "H:\\PenMarking\\eval\\qupath_annotation\\tiles"
+
+// Define image export type; valid values are JPG, PNG or null (if no image region should be exported with the mask)
+// Note: masks will always be exported as PNG
+def imageExportType = 'JPG'
+
+// Export each annotation
+annotations.each {
+    saveImageAndMask(pathOutput, server, it, downsample, imageExportType)
+}
+print 'Done!'
+
+/**
+ * Save extracted image region & mask corresponding to an object ROI.
+ *
+ * @param pathOutput Directory in which to store the output
+ * @param server ImageServer for the relevant image
+ * @param pathObject The object to export
+ * @param downsample Downsample value for the export of both image region & mask
+ * @param imageExportType Type of image (original pixels, not mask!) to export ('JPG', 'PNG' or null)
+ * @return
+ */
+def saveImageAndMask(String pathOutput, ImageServer server, PathObject pathObject, double downsample, String imageExportType) {
+    // Extract ROI & classification name
+    def roi = pathObject.getROI()
+    def pathClass = pathObject.getPathClass()
+    def classificationName = pathClass == null ? 'None' : pathClass.toString()
+    if (roi == null) {
+        print 'Warning! No ROI for object ' + pathObject + ' - cannot export corresponding region & mask'
+        return
+    }
+
+    // Create a region from the ROI
+    def region = RegionRequest.createInstance(server.getPath(), downsample, roi)
+
+    // Create a name
+    String name = String.format('%s_%s_(%.2f,%d,%d,%d,%d)',
+            server.getShortServerName(),
+            classificationName,
+            region.getDownsample(),
+            region.getX(),
+            region.getY(),
+            region.getWidth(),
+            region.getHeight()
+    )
+
+    // Request the BufferedImage
+    def img = server.readBufferedImage(region)
+
+    // Create a mask using Java2D functionality
+    // (This involves applying a transform to a graphics object, so that none needs to be applied to the ROI coordinates)
+    def shape = PathROIToolsAwt.getShape(roi)
+    def imgMask = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY)
+    def g2d = imgMask.createGraphics()
+    g2d.setColor(Color.WHITE)
+    g2d.scale(1.0/downsample, 1.0/downsample)
+    g2d.translate(-region.getX(), -region.getY())
+    g2d.fill(shape)
+    g2d.dispose()
+
+    // Create filename & export
+    if (imageExportType != null) {
+        pathOutput = pathOutput + File.separator + server.getShortServerName().take(server.getShortServerName().lastIndexOf('.'))
+        File newDir = new File(pathOutput)
+        if (!newDir.exists()) {
+            newDir.mkdirs()
+        }
+        def fileImage = new File(pathOutput, name + '.' + imageExportType.toLowerCase())
+        ImageIO.write(img, imageExportType, fileImage)
+    }
+    // Export the mask
+    def fileMask = new File(pathOutput, name + '-mask.png')
+    ImageIO.write(imgMask, 'PNG', fileMask)
+}
---
+++ src/wsi_annotation/QuPath_scripts/export_anno_tcga_xml.groovy
@@ -0,0 +1,150 @@
+/*
+ * Author: Jun Jiang (Jiang.Jun@mayo.edu), tested on QuPath 0.2.0-m2
+ *
+ * Script to export xml corresponding to all annotations of an Qupath project. Additionaly, a class label text and label ID tabel will be created.
+ * The xml meets the TCGA annotation format.  These website contains TCGA xml files
+ *   https://datadryad.org/resource/doi:10.5061/dryad.1g2nt41
+ *   https://datadryad.org/bitstream/handle/10255/dryad.172216/XML_TCGA_HG.zip?sequence=1
+ *       File details: https://datadryad.org/resource/doi:10.5061/dryad.1g2nt41/6
+ * Aperio Image Scope(a whole slide image viewer) can also provide annotations in this xml format, which means the xml may also be able to be import to Aperio, haven't test yet.
+ *  see also: ./AperioToQuPath.groovy
+ * This works by looping through all the images in a project, and checking for the existence of a data file.
+ * If a data file is found, read the hierarchy (no need to open the whole image), and write all the annotations to the xml files.
+ * If any package is missing, it will throw errors. You need to drag the missing jar onto the QuPath window, and you just need to do this only once. This step copies the jar into QuPath's jar directory
+ */
+
+import qupath.lib.images.servers.ImageServer
+import qupath.lib.objects.PathObject
+import qupath.lib.regions.RegionRequest
+import qupath.lib.roi.PathROIToolsAwt
+import qupath.lib.scripting.QP
+
+import javax.imageio.ImageIO
+import java.awt.Color
+import java.awt.image.BufferedImage
+
+import qupath.lib.objects.PathAnnotationObject
+import qupath.lib.roi.PointsROI
+import qupath.lib.roi.PolygonROI
+import qupath.lib.roi.AreaROI
+import qupath.lib.roi.RectangleROI
+import qupath.lib.geom.Point2
+
+import qupath.lib.gui.QuPathGUI
+import qupath.lib.io.PathIO
+import qupath.lib.objects.classes.PathClass
+import qupath.lib.gui.scripting.QPEx
+
+
+String xmlHead = '''<Annotations MicronsPerPixel="0.252100">
+  <Annotation Id="1" Name="" ReadOnly="0" NameReadOnly="0" LineColorReadOnly="0" Incremental="0" Type="4" LineColor="65280" Visible="1" Selected="1" MarkupImagePath="" MacroName="">
+    <Attributes>
+      <Attribute Name="Description" Id="0" Value=""/>
+    </Attributes>
+    <Regions>
+      <RegionAttributeHeaders>
+        <AttributeHeader Id="9999" Name="Region" ColumnWidth="-1"/>
+        <AttributeHeader Id="9997" Name="Length" ColumnWidth="-1"/>
+        <AttributeHeader Id="9996" Name="Area" ColumnWidth="-1"/>
+        <AttributeHeader Id="9998" Name="Text" ColumnWidth="-1"/>
+        <AttributeHeader Id="1" Name="Description" ColumnWidth="-1"/>
+      </RegionAttributeHeaders>\n'''
+String xmlTail = '''    </Regions>
+    <Plots/>
+  </Annotation>
+</Annotations>\n'''
+
+String regionStrHead = '''      <Region Id="%d" Type="%d" Text="%s" GeoShape="%s" Zoom="0.042148" Selected="0" ImageLocation="" ImageFocus="0" Length="74565.8" Area="213363186.2" LengthMicrons="18798.0" AreaMicrons="13560170.4" NegativeROA="0" InputRegionId="0" Analyze="1" DisplayId="1">
+        <Attributes/>
+        <Vertices>\n'''
+String regionStrTail = '''        </Vertices>
+      </Region>\n'''
+
+String vertexStr = '''          <Vertex X="%f" Y="%f"/>\n'''
+
+
+// Get the current project open in QuPath
+def project = QPEx.getProject()
+if (project == null) {
+    print 'No project open!'
+    return
+}
+
+String root_dir = "H:\\temp"
+
+// loop through all the entries to get the annotation label map
+File fp = new File(root_dir, "label_id.csv")
+def Anno_labels = []
+for (entry in project.getImageList()) {
+    def hierarchy = entry.readHierarchy()
+    def annotations = hierarchy.getFlattenedObjectList(null).findAll {it.isAnnotation()}
+    for (anno in annotations){
+        label_txt = anno.getPathClass()  //anno.getPathClass() return the class label
+        Anno_labels << label_txt
+    }    
+}
+def labels_set = Anno_labels.toSet()
+wrt_str = "Label,ID\n"
+labels_set.eachWithIndex{ item, idx -> wrt_str+="${item},${idx}\n"}
+fp.write(wrt_str)
+
+// get geometric type (rectangle, polygon, line ...) of the ROI from the class string
+def get_geoType(String roi_class_txt){
+    def last = roi_class_txt.tokenize('.')[-1]
+    return last.replace("ROI","")
+}
+
+int img_cnt = 0
+// Loop through all the entries in the project
+for (entry in project.getImageList()) {
+    String img_name = entry.getImageName()
+    println(sprintf('Processing %s', img_name))
+    
+    String uuid = img_name.split("\\.")[0]
+    File fh = new File(root_dir,uuid+".xml")
+    
+    img_cnt += 1
+    if (img_cnt > 2){
+        break
+    }
+    
+    // Read the hierarchy, but *not* anything else (don't need a full ImageData)
+    def hierarchy = entry.readHierarchy()
+
+    int nObjects = hierarchy.nObjects()
+    def annotations = hierarchy.getFlattenedObjectList(null).findAll {it.isAnnotation()}
+    // Print the total number of objects and number of annotations
+    println(sprintf('- Number of objects: %d, Number of annotations: %d', nObjects, annotations.size()))     
+    
+    int anno_cnt = 0
+    String wrt_str = xmlHead
+    for (anno in annotations){
+        anno_cnt += 1
+        int id = labels_set.findIndexOf {it == anno.getPathClass()}
+        
+        anno_geo_type = get_geoType(anno.getROI().getClass().toString())
+        region_str = String.format(regionStrHead, anno_cnt, id, anno.getPathClass(), anno_geo_type)  //anno.getPathClass() return the class label
+        wrt_str += region_str
+//        if (!(anno.getROI() instanceof PointsROI)){   // annotations are not points for offset calculation
+//            points = anno.getROI().getPolygonPoints()
+//            for (p in points){
+//                str_points = String.format(vertexStr, p.x, p.y)
+//                wrt_str += str_points
+//            }
+//        }
+        points = anno.getROI().getPolygonPoints()
+        for (p in points){
+            points_str = String.format(vertexStr, p.x, p.y)
+            wrt_str += points_str
+        }
+        wrt_str += regionStrTail
+    }
+    wrt_str += xmlTail
+    fh.write(wrt_str)
+}
+print "Done"
+
+
+
+
+
