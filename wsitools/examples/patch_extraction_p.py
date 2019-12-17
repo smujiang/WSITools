ddb6dff45b9e6cf5d57e2e2ce1ded045489fae73
Jun Jiang <smujiang@gmail.com>
2019-12-17 09:19:11 -0600

    tested, fine

--- wsitools/examples/patch_extraction_p.py
+++ wsitools/examples/patch_extraction_p.py
@@ -1,8 +1,8 @@
 from wsitools.tissue_detection.tissue_detector import TissueDetector
 from wsitools.patch_extraction.patch_extractor import ExtractorParameters, PatchExtractor
 
-wsi_fn = "/data/WSIs/MELF-Clean/8a26a55a78b947059da4e8c36709a828.tiff"
-output_dir = "/data/WSIs_extraction"
+wsi_fn = "/projects/shart/digital_pathology/data/PenMarking/WSIs/MELF/7470963d479b4576bc8768b389b1882e.tiff"
+output_dir = "/projects/shart/digital_pathology/data/PenMarking/WSIs/patches"
 
 tissue_detector = TissueDetector("LAB_Threshold", threshold=85)
 parameters = ExtractorParameters(output_dir, save_format='.png', sample_cnt=-1)
