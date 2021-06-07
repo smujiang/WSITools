98eb71552171bd052547f17ba9dea68e62ac659f
Jun Jiang <smujiang@gmail.com>
2021-06-07 14:19:45 -0500

    Update TMA_patch_extractor.py

--- wsitools/patch_extraction/TMA_patch_extractor.py
+++ wsitools/patch_extraction/TMA_patch_extractor.py
@@ -10,6 +10,9 @@ from PIL import Image, ImageDraw
 from scipy import ndimage
 from aicspylibczi import CziFile
 from pathlib import Path
+from skimage.measure import label, regionprops
+from skimage.segmentation import clear_border
+from skimage.morphology import closing, square
 
 logger = logging.getLogger(__name__)
 ch = logging.StreamHandler()
@@ -106,7 +109,9 @@ class PatchExtractor:
         :return: thumbnail_image
         """
         if case_info.get("ext").lower() == ".czi":
-            thumbnail = wsi_obj.read_mosaic(C=0, scale_factor=1/self.rescale_rate)
+            thumbnail = wsi_obj.read_mosaic(C=0, scale_factor=1/self.rescale_rate).squeeze()
+            img = np.swapaxes(thumbnail, 0, 1)
+            thumbnail = np.swapaxes(img, 1, 2)
         else:
             wsi_w, wsi_h = wsi_obj.dimensions
             thumb_size_x = wsi_w / self.rescale_rate
@@ -122,7 +127,6 @@ class PatchExtractor:
         :param wsi_thumb_mask: binary mask image with 1 for yes and 0 for no
         :return: coordinate array where the positive pixels are
         """
-
         wsi_thumb_mask = ndimage.binary_erosion(wsi_thumb_mask)
         pos_indices = np.where(wsi_thumb_mask > 0)
         loc_y = (np.array(pos_indices[0]) * self.rescale_rate).astype(np.int)
@@ -163,6 +167,44 @@ class PatchExtractor:
                     loc_y_selected.append(int(y))
         return [loc_x_selected, loc_y_selected]
 
+    def get_patch_locations_for_TMA(self, wsi_thumb_mask, level_downsample=1):
+        # import matplotlib.pyplot as plt
+        # plt.imshow(wsi_thumb_mask, cmap='gray')
+        # plt.show()
+        tissue_size = 3250  #TMA tissue size
+        tissue_thumbnail_diameter = tissue_size/self.rescale_rate # tissue circle diameter
+        tissue_thumbnail_area_filter = tissue_thumbnail_diameter**2 * 0.2
+        bw = closing(wsi_thumb_mask, square(3))
+        cleared = clear_border(bw)
+        label_image, num = label(cleared, return_num=True, background=0)
+        loc_x_selected = []
+        loc_y_selected = []
+        for region in regionprops(label_image):
+            if region.area >= tissue_thumbnail_area_filter:
+                pos_indices = np.where(label_image == region.label)
+                loc_y = (np.array(pos_indices[0]) * self.rescale_rate).astype(np.int)
+                loc_x = (np.array(pos_indices[1]) * self.rescale_rate).astype(np.int)
+                loc_x_list = []
+                loc_y_list = []
+                x_lim = [min(loc_x), max(loc_x)]
+                y_lim = [min(loc_y), max(loc_y)]
+                for x in range(x_lim[0], x_lim[1], int(self.stride*level_downsample)):
+                    for y in range(y_lim[0], y_lim[1], int(self.stride*level_downsample)):
+                        x_idx = int(x / self.rescale_rate)
+                        y_idx = int(y / self.rescale_rate)
+                        x_idx_1 = int((x+self.patch_size * level_downsample) / self.rescale_rate)
+                        y_idx_1 = int((y+self.patch_size * level_downsample) / self.rescale_rate)
+                        if x_idx_1 >= wsi_thumb_mask.shape[1]:
+                            x_idx_1 = x_idx
+                        if y_idx_1 >= wsi_thumb_mask.shape[0]:
+                            y_idx_1 = y_idx
+                        if np.count_nonzero(wsi_thumb_mask[y_idx:y_idx_1, x_idx:x_idx_1]) > 0:
+                            loc_x_list.append(int(x))
+                            loc_y_list.append(int(y))
+                loc_x_selected.append(loc_x_list)
+                loc_y_selected.append(loc_y_list)
+        return [loc_x_selected, loc_y_selected]
+
     def validate_extract_locations(self, case_info, locations, thumbnail, level_downsamples):
         """
         create a figure to validate the locations
@@ -191,6 +233,39 @@ class PatchExtractor:
             # thumbnail.show()
             print("Grids numbers in total: %d" % len(loc_x_selected))
             thumb_fn = os.path.join(self.log_dir, case_info["fn_str"]+"_extraction_grid_" + str(len(loc_x_selected)) + ".png")
+            thumbnail.save(thumb_fn)
+
+    def validate_TMA_extract_locations(self, case_info, locations, thumbnail, TMA_mask):
+        """
+        create a figure to validate the locations
+        :param locations:
+        :return:
+        """
+        if self.log_dir is None:
+            print("log dir is None, validation image will not be saved")
+            return
+        if not os.path.exists(self.log_dir):
+            try:
+                os.makedirs(self.log_dir,  exist_ok=True)
+            except OSError:
+                raise Exception("Can't create/access log_dir, unable to save validation image")
+        if os.path.exists(self.log_dir):
+            thumbnail = Image.fromarray(thumbnail)
+            draw = ImageDraw.Draw(thumbnail)
+            # [loc_x_selected, loc_y_selected] = locations
+            [loc_x_list, loc_y_list] = locations
+            cnt = 0
+            for loc_x_selected, loc_y_selected in zip(loc_x_list, loc_y_list):
+                for i in range(len(loc_x_selected)):
+                    cnt += 1
+                    xy = [int(loc_x_selected[i] / self.rescale_rate),
+                          int(loc_y_selected[i] / self.rescale_rate),
+                          int((loc_x_selected[i] + self.patch_size) / self.rescale_rate),
+                          int((loc_y_selected[i] + self.patch_size) / self.rescale_rate)]
+                    draw.rectangle(xy, outline='green')
+            # thumbnail.show()
+            print("Grids numbers in total: %d" % cnt)
+            thumb_fn = os.path.join(self.log_dir, case_info["fn_str"]+"_extraction_grid_" + str(cnt) + ".png")
             thumbnail.save(thumb_fn)
 
     @staticmethod
@@ -248,6 +323,22 @@ class PatchExtractor:
             tmp = (case_info["fn_str"] + "_%d_%d_%s" + self.save_format) % (
                 int(patch_loc[0]), int(patch_loc[1]), label_text)
         return os.path.join(self.save_dir, case_info["fn_str"], tmp)
+
+    def generate_TMA_patch_fn(self, case_info, TMA_idx, patch_loc, label_text=None):
+        """
+        Creates the filenames, if we save the patches as jpg/png files.
+
+        :param case_info: likely a UUID or sample name
+        :param patch_loc: tuple of (x, y) locations for where the patch came from
+        :param label_text: #TODO: Need to define this
+        :return: outputFileName
+        """
+        if label_text is None:
+            tmp = (case_info["fn_str"] + "_%d_%d" + self.save_format) % (int(patch_loc[0]), int(patch_loc[1]))
+        else:
+            tmp = (case_info["fn_str"] + "_%d_%d_%s" + self.save_format) % (
+                int(patch_loc[0]), int(patch_loc[1]), label_text)
+        return os.path.join(self.save_dir, case_info["fn_str"], str(TMA_idx), tmp)
 
     def generate_tfRecords_fp(self, case_info):
         """
@@ -386,8 +477,73 @@ class PatchExtractor:
         return patch_cnt
 
     def save_CZI_patches(self, wsi_obj, case_info, indices):
+        patch_cnt = 0
+        if self.with_feature_map:
+            tf_writer, tf_fn = self.generate_tfRecords_fp(case_info)
+        if case_info.get("ext").lower() == ".czi":
+            bbox = wsi_obj.scene_bounding_box()  # acispylibczi version = 2.8.0
+            # bbox = wsi_obj.get_mosaic_scene_bounding_box()  # acispylibczi version >= 3.0.1, still have issue
+            [loc_x_list, loc_y_list] = indices
+            for TMA_idx, loc_x in enumerate(loc_x_list):
+                loc_y = loc_y_list[TMA_idx]
+                for idx, lx in enumerate(loc_x):
+                    mosaic_data = wsi_obj.read_mosaic(C=0, region=(bbox[0]+loc_x[idx], bbox[1]+loc_y[idx], self.patch_size, self.patch_size), scale_factor=1.0)
+                    img = np.swapaxes(mosaic_data, 0, 1)  #TODO: may not necessary in higher version
+                    patch = np.swapaxes(img, 1, 2)  #TODO: may not necessary
+                    patch = Image.fromarray(patch)
+                    if self.patch_rescale_to:
+                        patch = patch.resize([self.patch_rescale_to, self.patch_rescale_to])
+                    # Only print out the patches that contain tissue in them (e.g. Content Rich)
+                    Content_rich = True
+                    if self.patch_filter_by_area:  # if we need to filter the image patch
+                        Content_rich = self.filter_by_content_area(np.array(patch), area_threshold=self.patch_filter_by_area)
+                    if Content_rich:
+                        patch_cnt += 1
+                        if self.with_anno:
+                            label_id, label_txt = self.get_patch_label([loc_x[idx], loc_y[idx]])
+                        else:
+                            label_txt = None
+                            label_id = -1  # can't delete this line, it will be used if save patch into tfRecords
+                        if self.with_feature_map:  # Append data to tfRecord file
+                            # TODO: maybe need to find another way to do this
+                            values = []
+                            for eval_str in self.feature_map.eval_str:
+                                values.append(eval(eval_str))
+                            features = self.feature_map.update_feature_map_eval(values)
+                            example = tf.train.Example(
+                                features=tf.train.Features(feature=features))  # Create an example protocol buffer
+                            tf_writer.write(example.SerializeToString())  # Serialize to string and write on the file
+                            logger.info('\rWrote {} to tfRecords '.format(patch_cnt))
+                            sys.stdout.flush()
+                        else:  # save patch to jpg, with label text and id in file name
+                            # if logger.DEBUG == logger.root.level:
+                            #     import matplotlib.pyplot as plt
+                            #     plt.figure(1)
+                            #     plt.imshow(patch)
+                            #     plt.show()
+                            fn = self.generate_TMA_patch_fn(case_info, TMA_idx, (loc_x[idx], loc_y[idx]), label_text=label_txt)
+                            if not os.path.exists(os.path.split(fn)[0]):
+                                os.makedirs(os.path.split(fn)[0])
+                            if self.save_format == ".jpg":
+                                patch.save(fn)
+                            elif self.save_format == ".png":
+                                patch.convert("RGBA").save(fn)
+                            else:
+                                raise Exception("Can't recognize save format")
+                            logger.info('\rWrote {} to image files '.format(patch_cnt))
+                            sys.stdout.flush()
+                        if self.sample_cnt == patch_cnt:
+                            if self.with_feature_map:
+                                tf_writer.close()
+                            return patch_cnt
+                    else:
+                        logger.debug("No content found in image patch x: {} y: {}".format(loc_x[idx], loc_y[idx]))
+                if self.with_feature_map:
+                    tf_writer.close()
+        else:
+            raise Exception("This function can only extract image patches from Zessis images")
+        return patch_cnt
 
-        return 0
 
     # get image patches and write to files
     def save_patches(self, wsi_obj, case_info, indices):
@@ -409,14 +565,13 @@ class PatchExtractor:
                 mosaic_data = wsi_obj.read_mosaic(C=0, region=(bbox[0]+loc_x[idx], bbox[1]+loc_y[idx], self.patch_size, self.patch_size), scale_factor=1.0)
                 img = np.swapaxes(mosaic_data, 0, 1)
                 patch = np.swapaxes(img, 1, 2)
+                patch = Image.fromarray(patch)
             else:
                 patch = wsi_obj.read_region((loc_x[idx], loc_y[idx]),
                                             self.extract_layer,
                                             (self.patch_size, self.patch_size)
                                             ).convert("RGB")
             if self.patch_rescale_to:
-                if isinstance(patch, np.ndarray):
-                    patch = Image.fromarray(patch)
                 patch = patch.resize([self.patch_rescale_to, self.patch_rescale_to])
             # Only print out the patches that contain tissue in them (e.g. Content Rich)
             Content_rich = True
@@ -479,12 +634,12 @@ class PatchExtractor:
             wsi_obj, case_info = self.get_case_info(wsi_fn)
             wsi_thumb = self.get_thumbnail(wsi_obj, case_info)  # get the thumbnail
             wsi_thumb_mask = self.tissue_detector.predict(wsi_thumb)  # get the foreground thumbnail mask
-            extract_locations = self.get_patch_locations(wsi_thumb_mask, wsi_obj.level_downsamples)
-            self.validate_extract_locations(case_info, extract_locations, wsi_thumb, wsi_obj.level_downsamples)
+            extract_locations = self.get_patch_locations_for_TMA(wsi_thumb_mask)
+            self.validate_TMA_extract_locations(case_info, extract_locations, wsi_thumb, wsi_thumb_mask)
             return self.save_CZI_patches(wsi_obj, case_info, extract_locations)
         else:  # suppose other images are able to be processed with OpenSlide package
             wsi_obj, case_info = self.get_case_info(wsi_fn)
-            wsi_thumb = self.get_thumbnail(wsi_obj)  # get the thumbnail
+            wsi_thumb = self.get_thumbnail(wsi_obj, case_info)  # get the thumbnail
             wsi_thumb_mask = self.tissue_detector.predict(wsi_thumb)  # get the foreground thumbnail mask
             extract_locations = self.get_patch_locations(wsi_thumb_mask, wsi_obj.level_downsamples)
             self.validate_extract_locations(case_info, extract_locations, wsi_thumb, wsi_obj.level_downsamples)
@@ -517,28 +672,19 @@ class PatchExtractor:
 
 if __name__ == "__main__":
     from wsitools.tissue_detection.tissue_detector import TissueDetector  # import dependent packages
-    from wsitools.patch_extraction.feature_map_creator import FeatureMapCreator
-    from wsitools.wsi_annotation.region_annotation import AnnotationRegions
-
-    # wsi_fn = "/projects/shart/digital_pathology/data/PenMarking/WSIs/MELF/e39a8d60a56844d695e9579bce8f0335.tiff"  # WSI file name
-    # output_dir = "/projects/shart/digital_pathology/data/PenMarking/temp"
+    # from wsitools.patch_extraction.feature_map_creator import FeatureMapCreator
+    # from wsitools.wsi_annotation.region_annotation import AnnotationRegions
     #
+
+
+    wsi_fn = "/infodev1/non-phi-data/junjiang/OvaryCancer/StromaReaction/TMA_WSIs/OvarianTMA_Goode_H&E.czi"  # WSI file name
+    # wsi_fn = "/infodev1/non-phi-data/junjiang/OvaryCancer/WSIs/OCMC-017.svs"
+    output_dir = "/infodev1/non-phi-data/junjiang/OvaryCancer/StromaReaction/TMA_WSIs_patches"
+    log_dir = "/infodev1/non-phi-data/junjiang/OvaryCancer/StromaReaction/log"
     # tissue_detector = TissueDetector("LAB_Threshold", threshold=85)  #
-    # # fm = FeatureMapCreator("./feature_maps/basic_fm_PL_eval.csv")  # use this template to create feature map
-    # # xml_fn = "/projects/shart/digital_pathology/data/PenMarking/annotations/temp/e39a8d60a56844d695e9579bce8f0335.xml"
-    # class_label_id_csv = "/projects/shart/digital_pathology/data/PenMarking/annotations/temp/label_id.csv"
-    # annotations = AnnotationRegions(xml_fn, class_label_id_csv)
-    # parameters = ExtractorParameters(output_dir, save_format='.tfrecord', sample_cnt=-1)
-    # patch_extractor = PatchExtractor(tissue_detector, parameters=parameters, feature_map=fm,
-    #                                  annotations=annotations)
-    # patch_num = patch_extractor.extract(wsi_fn)
+    tissue_detector = TissueDetector("LAB_Threshold", threshold=60)
 
-    wsi_fn = "\\\\mfad\\researchmn\\HCPR\\HCPR-GYNECOLOGICALTUMORMICROENVIRONMENT\\WSIs\\OCMC-016.svs"  # WSI file name
-    output_dir = "H:\\OvarianCancer\\ImageData\\Patches\\OCMC-016"
-    log_dir = "H:\\OvarianCancer\\ImageData\\Patches\\OCMC-016_log"
-    tissue_detector = TissueDetector("LAB_Threshold", threshold=85)  #
-
-    parameters = ExtractorParameters(output_dir, patch_size=500, stride=500, extract_layer=0, patch_filter_by_area=0.3,
+    parameters = ExtractorParameters(output_dir, log_dir=log_dir, patch_size=500, stride=500, extract_layer=0, patch_filter_by_area=0.3,
                                      save_format='.jpg', sample_cnt=-1)
 
     '''
