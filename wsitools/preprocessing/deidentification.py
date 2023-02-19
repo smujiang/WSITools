e0ce378e678c2c905499f39c8a5336cb083f391d
Jiang <jiang.jun@mayo.edu>
2023-02-19 15:01:18 -0600

    add deidentification example

---
+++ wsitools/preprocessing/deidentification.py
@@ -0,0 +1,25 @@
+import struct
+import tifffile
+
+fn = "/infodev1/non-phi-data/junjiang/OvaryCancer/WSIs/tobe_deidentified.svs"
+with tifffile.TiffFile(fn, mode='r+b') as svs:
+    assert svs.is_svs
+    fh = svs.filehandle
+    tiff = svs.tiff
+    for page in svs.pages[::-1]:
+        if page.subfiletype not in (1, 9):
+            break  # not a label or macro image
+        # zero image data in page
+        for offset, bytecount in zip(page.dataoffsets, page.databytecounts):
+            fh.seek(offset)
+            fh.write(b'\0' * bytecount)
+        # seek to position where offset to label/macro page is stored
+        previous_page = svs.pages[page.index - 1]  # previous page
+        fh.seek(previous_page.offset)
+        tagno = struct.unpack(tiff.tagnoformat, fh.read(tiff.tagnosize))[0]
+        offset = previous_page.offset + tiff.tagnosize + tagno * tiff.tagsize
+        fh.seek(offset)
+        # terminate IFD chain
+        fh.write(struct.pack(tiff.offsetformat, 0))
+        print(f'wiped {page}')
+
