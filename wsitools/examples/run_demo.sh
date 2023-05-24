e2f0ae9a5b526b49a3a6f19a8a7c9cdeb1e332ec
Jiang <jiang.jun@mayo.edu>
2023-05-24 09:54:25 -0500

    Update run_demo.sh

--- wsitools/examples/run_demo.sh
+++ wsitools/examples/run_demo.sh
@@ -4,32 +4,32 @@
 # Timing 40x, average file size
 output_root=/lus/grand/projects/gpu_hack/mayopath/Jun/data
 mkdir $output_root/256_64
-time python patch_extraction_p_args.py -s 256 -o $output_root/256_64 -n 64 >> $output_root/log_256_64.txt
+python -m cProfile -o $output_root/256_64/256_64.stats patch_extraction_p_args.py -s 256 -o $output_root/256_64 -n 64 >> $output_root/log_256_64.txt
 
 mkdir $output_root/384_64
-time python patch_extraction_p_args.py -s 384 -o $output_root/384_64 -n 64 >> $output_root/log_384_64.txt
+python -m cProfile -o $output_root/384_64/384_64.stats patch_extraction_p_args.py -s 384 -o $output_root/384_64 -n 64 >> $output_root/log_384_64.txt
 
 mkdir $output_root/512_64
-time python patch_extraction_p_args.py -s 512 -o $output_root/512_64 -n 64 >> $output_root/log_512_64.txt
+python -m cProfile -o $output_root/512_64/512_64.stats patch_extraction_p_args.py -s 512 -o $output_root/512_64 -n 64 >> $output_root/log_512_64.txt
 
 mkdir $output_root/1024_64
-time python patch_extraction_p_args.py -s 1024 -o $output_root/1024_64 -n 64 >> $output_root/log_1024_64.txt
+python -m cProfile -o $output_root/1024_64/1024_64.stats patch_extraction_p_args.py -s 1024 -o $output_root/1024_64 -n 64 >> $output_root/log_1024_64.txt
 
 mkdir $output_root/2048_64
-time python patch_extraction_p_args.py -s 2048 -o $output_root/2048_64 -n 64 >> $output_root/log_2048_64.txt
+python -m cProfile -o $output_root/2048_64/2048_64.stats patch_extraction_p_args.py -s 2048 -o $output_root/2048_64 -n 64 >> $output_root/log_2048_64.txt
 
 ############## different number of process ##################
 mkdir $output_root/512_8
-time python patch_extraction_p_args.py -s 512 -o $output_root/512_8 -n 8 >> $output_root/log_512_8.txt
+python -m cProfile -o $output_root/512_8/512_8.stats patch_extraction_p_args.py -s 512 -o $output_root/512_8 -n 8 >> $output_root/log_512_8.txt
 
 mkdir $output_root/512_16
-time python patch_extraction_p_args.py -s 512 -o $output_root/512_16 -n 16 >> $output_root/log_512_16.txt
+python -m cProfile -o $output_root/512_16/512_16.stats patch_extraction_p_args.py -s 512 -o $output_root/512_16 -n 16 >> $output_root/log_512_16.txt
 
 mkdir $output_root/512_32
-time python patch_extraction_p_args.py -s 512 -o $output_root/512_32 -n 32 >> $output_root/log_512_32.txt
+python -m cProfile -o $output_root/512_32/512_32.stats patch_extraction_p_args.py -s 512 -o $output_root/512_32 -n 32 >> $output_root/log_512_32.txt
 
 mkdir $output_root/512_128
-time python patch_extraction_p_args.py -s 512 -o $output_root/512_128 -n 128 >> $output_root/log_512_128.txt
+python -m cProfile -o $output_root/512_128/512_128.stats patch_extraction_p_args.py -s 512 -o $output_root/512_128 -n 128 >> $output_root/log_512_128.txt
 
 
 
