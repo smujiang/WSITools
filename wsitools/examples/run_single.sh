908d67f801e207b0c777f849854342270c39a087
Jiang <jiang.jun@mayo.edu>
2023-05-24 12:17:09 -0500

    Update run_single.sh

--- wsitools/examples/run_single.sh
+++ wsitools/examples/run_single.sh
@@ -4,7 +4,7 @@
 # Timing 40x, average file size
 output_root=/lus/grand/projects/gpu_hack/mayopath/Jun/data
 mkdir $output_root/512_64
-python -m cProfile -o $output_root/512_64/512_64_s.stats patch_extraction_p_args.py -s 512 -o $output_root/512_64 -n 64 >> $output_root/log_512_64_s.txt
+python -m cProfile -o $output_root/512_64/512_64_s.stats patch_extraction_p_args_single.py -s 512 -o $output_root/512_64 -n 1 >> $output_root/log_512_64_s.txt
 
 
 
