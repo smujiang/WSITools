0c13a49e584cddfca07e68bd160c9f5831792238
Jiang <jiang.jun@mayo.edu>
2023-05-25 22:16:30 -0500

    Create profile_local.sh

---
+++ wsitools/patch_extraction/profile_local.sh
@@ -0,0 +1,25 @@
+# Configuration ------------------------
+
+PROFILE_DIR="$MayoPath/Jun/data/test"
+mkdir -p "$PROFILE_DIR"
+PROFILE_OUT="$PROFILE_DIR/patch_extract_local_gpu.prf" # Change to set profiling differently
+
+export PMI_LOCAL_RANK=1
+
+# Set up environment
+source "$MayoPath/init_mmae.sh"
+
+(
+cd ..
+
+# Nsys profile from https://dev-discuss.pytorch.org/t/using-nsight-systems-to-profile-gpu-workload/59
+nsys profile -w true \
+	-t cuda,nvtx,osrt,cudnn,cublas \
+	-s cpu \
+	--capture-range=cudaProfilerApi \
+	--capture-range-end=stop \
+	--cudabacktrace=true \
+	-x true \
+	-o $PROFILE_OUT \
+python -m patch_extractor.py \
+	)
